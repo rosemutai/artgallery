7ceee90afe0e4533552aca157b0274e18bef4ccf
Brian_OB <25736452+Brianonchari@users.noreply.github.com>
2020-06-27 16:10:42 +0300

    commit

--- src/account/views.py
+++ src/account/views.py
@@ -126,5 +126,5 @@ def create_profile_view(request):
 
 			context['form'] = form
 
-	return render(request, "account/create_profile.html", context)
+	return render(request, "account/edit_account.html", context)
 
